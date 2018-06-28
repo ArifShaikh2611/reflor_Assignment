fe59a5aa3358d363851e4dd6ea619c8830f4fdfa
arif shaikh <rf.shaikh42@gmail.com>
2018-06-28 17:56:29 +0530

    completed the changes

--- src/components/header.js
+++ src/components/header.js
@@ -11,23 +11,39 @@ class Header extends Component {
 
   render(){
     return(
-      <nav className="navbar navbar-expand-sm navBgColor"  id="nav-menu-container">
-      <div>
-        <ul className="navbar-nav order-3">
-          <li className="nav-item">
-            <Link to="/view" ><span style={navColor}>View</span></Link>
-          </li>
-          <li className="nav-item ml-3">
-          
-          <Link to="/add" ><span style={navColor}>Add</span></Link>
-          </li>
-          
-      </ul>
-      </div>
-    </nav>
+      <header id="header" className="header-fixed navBgColor">
+        <div className="container-fluid">
+        <div style={{display:'inline-block'}}>
+        <Link to="/view" ><img src={'../images/logo.jpg'} alt="logo image"/></Link>
+          </div>
+          <nav className="navbar navbar-expand-sm"  id="nav-menu-container">
+            <div>
+                <ul className="navbar-nav order-3">
+                  <li className="nav-item">
+                    <Link to="/view" ><span style={navColor}>View</span></Link>
+                  </li>
+                  <li className="nav-item ml-3">
+                  
+                  <Link to="/add" ><span style={navColor}>Add</span></Link>
+                  </li>
+                  
+              </ul>
+            </div>
+          </nav>
+        </div>
+    </header>
     );
   }
 }
+
+
+
+
+
+
+
+
+
 
 
 export default Header;
